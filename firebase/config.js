f7bc6fe932c64094a50adc2fe0d8744c4ac8cb1c
Omji Dubey <omjidubey9453@outlook.com>
2026-04-13 16:27:35 +0000

    Add firebase config, index.html files

---
+++ firebase/config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth";
+import {getFirestore} from "firebase/firestore"
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCQgWp9vg8RM688_yFMDUuZW6SWiYnwFMU",
+  authDomain: "e-learning-platform-12.firebaseapp.com",
+  projectId: "e-learning-platform-12",
+  storageBucket: "e-learning-platform-12.firebasestorage.app",
+  messagingSenderId: "697719521905",
+  appId: "1:697719521905:web:13eceb3328defe3b9851d0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const firestore = getFirestore(app);
